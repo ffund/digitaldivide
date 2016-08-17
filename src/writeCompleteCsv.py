0d279dd5afba9fc168f6e31264b49a9fc93cafdf
Caleb Salzberg <calebsmithsalzberg@gmail.com>
2016-08-16 20:29:41 -0400

    final csv

--- src/writeCompleteCsv.py
+++ src/writeCompleteCsv.py
@@ -1,22 +1,31 @@
 import sys
 import pandas as pd
 import numpy as np
-import price
+import priceForEachHouse
 import numpy 
-
 import math
 
 pd.set_option('max_columns', 50)
 
-allcsv = pd.read_csv('workingcompact.csv')
-URS = pd.read_csv('newURS.csv')
+allcsv = pd.read_csv('dat/household.csv')
+URS = pd.read_csv('dat/newURS.csv')
 
 houseID = 405
 
-p = price.householdPrice(houseID, allcsv, URS)
+p = priceForEachHouse.householdPrice(houseID, allcsv, URS)
 
 l = list(set(allcsv.unit_id))
-prices = [price.householdPrice(litem, allcsv, URS) for litem in l]
+prices = [priceForEachHouse.householdPrice(litem, allcsv, URS) for litem in l]
 sum(numpy.logical_not(numpy.isnan(prices)))
 
-allcsv['price'] = pd.Series(prices, index=allcsv.index)
+addprice=[]
+x=0
+for i in l:
+	addprice.append({'unit_id':i, 'price':prices[x]})
+	x+=1
+#allcsv['price'] = pd.Series(prices, index=allcsv.index)
+pricefinal=pd.DataFrame(addprice)
+new=pd.merge(allcsv,pricefinal, on='unit_id', how='inner')
+new.to_csv('full.csv')
+print 'merged'
+
