a27e545058731d91c7d262c732570912f02ab308
Fraida Fund <ffund@nyu.edu>
2016-08-30 00:01:10 -0400

    updates to experiment script:
    Make postboot execute the apt-get and netem commands separately
    Save both Rspec and JSON in a file with house ID in it
    Better error handling when no such house exists
    More informative printout (tell me household characteristics)
    Add information about SATELLITE to the help output

--- src/finalexperiment.py
+++ src/finalexperiment.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--state", help="Specify a state from which to draw a sample household. (two letters)")
 parser.add_argument("--houseid", help="Specify a household ID from the Measuring Broadband America data set.")
 parser.add_argument("--price-range", help="A number, followed by a '-', followed by a number greater than the first (number is amount in dollars). Only whole numbers please.")
-parser.add_argument("--technology", help="CABLE, FIBER, or DSL.")
+parser.add_argument("--technology", help="CABLE, FIBER, SATELLITE, or DSL.")
 args = parser.parse_args()
 
 # Json helper function
@@ -78,20 +78,24 @@ def getJSON(delay, jitterdown, jitterup, upspeed, downspeed, percentloss, house)
 # Reading the database
 allcsv = pd.read_csv('full.csv')
 
-if args.houseid:
-	house = int(args.houseid)
-	houseinfo = allcsv[allcsv.unit_id == house]
-if not args.houseid:
-	if args.state:
-		allcsv=allcsv[allcsv.STATE == args.state]
-	if args.price_range:
-		allcsv=allcsv[allcsv.price >= int(args.price_range.split("-")[0])]
-		allcsv=allcsv[allcsv.price <= int(args.price_range.split("-")[1])]
-	if args.technology:
-		allcsv=allcsv[allcsv.TECHNOLOGY == args.technology.upper()]
+try:
+	if args.houseid:
+		house = int(args.houseid)
+		houseinfo = allcsv[allcsv.unit_id == house]
+	if not args.houseid:
+		if args.state:
+			allcsv=allcsv[allcsv.STATE == args.state]
+		if args.price_range:
+			allcsv=allcsv[allcsv.price >= int(args.price_range.split("-")[0])]
+			allcsv=allcsv[allcsv.price <= int(args.price_range.split("-")[1])]
+		if args.technology:
+			allcsv=allcsv[allcsv.TECHNOLOGY == args.technology.upper()]
 
-	houseinfo = allcsv.sample(n=1)
-	house = int(houseinfo.unit_id)
+		houseinfo = allcsv.sample(n=1)
+		house = int(houseinfo.unit_id)
+except ValueError:
+	print "\nThere are no households meeting the criteria you have set.\n"
+	sys.exit()
 
 # Extract data for the selected household
 splitup = houseinfo[['Percent Loss','Latency','jitter_up','jitter_down','Speed_up','Speed_down']]
@@ -99,7 +103,9 @@ splitup = houseinfo[['Percent Loss','Latency','jitter_up','jitter_down','Speed_u
 if splitup.empty:
 	# Get a random sample to suggest house id's to try
 	sample_ids = allcsv.sample(n=5)['unit_id'].tolist()
-	raise ValueError("The inputted house_id is not in the database.\nTry these: %s" % ", ".join(map(str, sample_ids)))
+	print "\nThere is no such houseid in the database."
+	print "Try these: %s\n" % ", ".join(map(str, sample_ids))
+        sys.exit()
 
 # Reading parameters, conversions
 percentloss = float(splitup['Percent Loss'])
@@ -112,12 +118,21 @@ downspeed = int(round(splitup['Speed_down'] * 0.008))
 upspeed = int(round(splitup['Speed_up'] * 0.008))
 speed = max(downspeed, upspeed)
 
+print "Selected household %d has the following characteristics:" % house
+print "--------------------------------------------------------"
+print " Upload rate (kbps)    | %d                             " % upspeed
+print " Download rate (kbps)  | %d                             " % downspeed
+print " Round-trip delay (ms) | %f                             " % (delay*2)
+print " Uplink jitter (ms)    | %f                             " % jitterup
+print " Downlink jitter (ms)  | %f                             " % jitterdown
+print " Packet loss (%%)       | %f                             " % percentloss
+print "--------------------------------------------------------"
+
+
 def netem_template(delay, jitter, percentloss, speed):
 	statements = [
 	"sudo tc qdisc add dev eth1 root handle 1:0 netem delay %0.6fms %0.6fms loss %0.6f%%" % (delay, jitter, percentloss),
-	"sudo tc qdisc add dev eth1 parent 1:1 handle 10: tbf rate %dkbit limit 500000000 burst 100000" % speed,
-	"sudo apt-get -y install iperf", # for testing
-	"touch ready" # to know the command is complete, check for ~/user/name/ready
+	"sudo tc qdisc add dev eth1 parent 1:1 handle 10: tbf rate %dkbit limit 500000000 burst 100000" % speed
 	]
 	return "; ".join(statements)
 
@@ -145,6 +160,7 @@ for i in range(len(con)):
 
 	igvm = IGX.XenVM(nodename)
 	igvm.addService(PG.Execute(shell="/bin/sh", command=netem))
+	igvm.addService(PG.Execute(shell="/bin/sh", command="sudo apt-get update; sudo apt-get -y install iperf"))
 	vms.insert(i, igvm)
 
 	# Generate interfaces and connections for this VM
@@ -160,11 +176,11 @@ for l in links:
 	r.addResource(l)
 
 # Determining the location for rspec
-rspec = os.path.join(os.getcwd(), "miniexperiment.xml")
+rspec = os.path.join(os.getcwd(), "house-%d.xml" % house)
 r.writeXML(rspec)
 print "Rspec written to %s" % rspec
 
-jfile = os.path.join( os.getcwd(), "house%d.json" % house)
+jfile = os.path.join( os.getcwd(), "house-%d.json" % house)
 f = open(jfile, 'w')
 json.dump(getJSON(delay, jitterdown, jitterup, upspeed, downspeed, percentloss, house), f)
 f.close()
