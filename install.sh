6b0d0d3c8d5415f568649449375e10012e22184e
Fraida Fund <ffund@nyu.edu>
2016-08-29 20:32:07 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+# This script installs prerequisites on an Ubuntu 14.04 installation
+# Run it as: bash install.sh
+
+# Install geni-lib prerequisite: 
+# From http://geni-lib.readthedocs.io/en/stable/intro/ubuntu.html
+sudo apt-get update
+sudo apt-get -y install mercurial python-pip python-m2crypto python-dateutil python-lxml \
+  libxmlsec1 xmlsec1 libxmlsec1-openssl python-setuptools python-openssl libffi-dev 
+
+cd ~  
+hg clone http://bitbucket.org/barnstorm/geni-lib
+cd geni-lib
+hg update -C 0.9-DEV
+sudo python setup.py install
+cd ~
+
+# We need pandas version >= 0.16.1. 
+# The version in Ubuntu 14.04 repository is too old. So we get it from PyPI
+sudo pip install pandas
