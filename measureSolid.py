f07637eb6daeae79dd4194323d3647b5f76551f5
css493 <css493@house-27079.work.ch-geni-net.genirack.nyu.edu>
2016-12-01 16:24:16 -0500

    makes csv from data

--- measureSolid.py
+++ measureSolid.py
@@ -1,3 +1,4 @@
+tocsv = "type,trial,measure \n"
 for i in range(2):
     everything = open("tmp/dlrate-"+str(i)+".txt", "r").read()
     allline = everything[everything.rfind("[SUM]"):everything.rfind("receiver")]
@@ -11,11 +12,10 @@ for i in range(2):
     ignorenum10 = float(ignore10[ignore10.find("Bytes")+5: ignore10.rfind("bits")-2].strip())
     #print ignorenum10
     no10 = (allnum*12 - ignorenum5 - ignorenum10) / 10
-    print no10
+    tocsv+="dlrate, "+str(i)+", "+str(no10)+"\n"
 
-for i in range(2):
-    everything = open("tmp/ulrate-"+str(i)+".txt", "r").read()
-    allline = everything[everything.rfind("[SUM]"):everything.rfind("receiver")]
+    everything2 = open("tmp/ulrate-"+str(i)+".txt", "r").read()
+    allline = everything2[everything2.rfind("[SUM]"):everything2.rfind("receiver")]
     allnum = float(allline[allline.find("Bytes")+5: allline.rfind("bits")-2].strip())
     #print allnum
     ignore5 = everything[everything.find("[SUM]"):everything.find("- -")]
@@ -26,10 +26,11 @@ for i in range(2):
     ignorenum10 = float(ignore10[ignore10.find("Bytes")+5: ignore10.rfind("bits")-2].strip())
     #print ignorenum10
     no10 = (allnum*12 - ignorenum5 - ignorenum10) / 10
-    print no10
-for i in range(2):
-    everything = open("tmp/ping-"+str(i)+".txt", "r").read()
-    line = everything[everything.rfind("="):].split("/")
+    tocsv+="ulrate, "+str(i)+", "+str(no10)+"\n"
+
+    everything3 = open("tmp/ping-"+str(i)+".txt", "r").read()
+    line = everything3[everything3.rfind("="):].split("/")
     ping = line[1]
-    print ping
+    tocsv+="latency, "+str(i)+", "+str(ping)+"\n"
 #PROBLEM: not sure if kbps or mbps. easy fix, but remember to do
+print tocsv
