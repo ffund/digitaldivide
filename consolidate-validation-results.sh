8c76d29278f922ff6ce9d50a0329c395ffb32bcd
Fraida Fund <ffund@nyu.edu>
2016-12-01 18:22:01 -0500

    Updates to scripts for validation and data analysis

---
+++ consolidate-validation-results.sh
@@ -0,0 +1,31 @@
+
+OUTFILE="test.csv"
+echo "type,trial,measure"
+
+for tputtype in "dl" "ul"; do
+  for file in tmp/"$tputtype"rate-*; do
+    lines=$(cat "$file" | grep "SUM" | head -12 | tail -10 )
+    mBytes=$(echo -e "$lines" | grep "MBytes" |  awk '{sum+=$4} END {print sum}')
+    kBytes=$(echo -e "$lines" | grep "KBytes" |  awk '{sum+=$4} END {print sum}')
+    Bytes=$(echo $lines | grep " Bytes" |  awk '{sum+=$4} END {print sum}')
+    [[  -z  $mBytes  ]] && mBytes=0
+    [[  -z  $kBytes  ]] && kBytes=0
+    [[  -z  $Bytes  ]] && Bytes=0
+    begintime=$(cat "$file" | grep "SUM" | head -3 | tail -1 | awk '{print $2}' | cut -f1 -d'-')
+    endtime=$(cat "$file" | grep "SUM" | head -12 | tail -1 | awk '{print $2}' | cut -f2 -d'-')
+    throughput=$(bc <<< "scale=5; 8*($mBytes*1000 + $kBytes + $Bytes/1000)/($endtime - $begintime)" )
+    mtype="$tputtype"rate
+    trial=$(echo $file | awk -F'[-.]' '{print $2}')
+
+    echo "$mtype,$trial,$throughput"
+  done
+done
+
+for file in tmp/ping*; do
+
+  mtype="latency"
+  latency=$(cat tmp/ping-1.txt | tail -1 | cut -d'/' -f5)
+  trial=$(echo $file | awk -F'[-.]' '{print $2}')
+  echo "$mtype,$trial,$latency"
+done
+
--- validate.sh
+++ validate.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-# Install software prerequisites for validation
-sudo apt-get update
-sudo apt-get -y install iperf software-properties-common python-software-properties
-sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
-sudo apt-get update 
-sudo apt-get -y install iperf3
+IPERF_OK=$(dpkg-query -W --showformat='${Status}\n' iperf3 | grep "install ok installed")
+if [ "" == "$IPERF_OK" ]; then
+  # Install software prerequisites for validation
+  sudo apt-get update
+  sudo apt-get -y install iperf software-properties-common python-software-properties
+  sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
+  sudo apt-get update 
+  sudo apt-get -y install iperf3
+fi
 
 # Linux kernel tuning TODO
 
-for trial in $(seq 0 1); do
+for trial in $(seq 0 10); do
+  echo '[' `date` ']' " Ping test for trial $trial"
   # Test latency
   ping server -i 0.2 -c 100 > "ping-$trial.txt"
 
   # Test UL/DL speed
   # Server must be running 'iperf3 -s'
-  iperf3 -c server -i 5 -t 60 -P 3 -w 50k        > "ulrate-$trial.txt"
-  iperf3 -c server -i 5 -t 60 -P 3 -w 50k -R     > "dlrate-$trial.txt"
+  echo '[' `date` ']' " UL/DL throughput test for trial $trial"
+  iperf3 -c server -i 5 -t 60 -P 3 -w 50k --format k        > "ulrate-$trial.txt"
+  iperf3 -c server -i 5 -t 60 -P 3 -w 50k -R --format k     > "dlrate-$trial.txt"
 
   # Test UL/DL jitter and packet loss
   # Server must be running 'iperf -s -u'
+  echo '[' `date` ']' " Jitter/loss test for trial $trial"
+  iperf -c server -u -b 100k -t 60 -l 180 -r  > "udp-$trial.txt"
 done
 #for trial in $(seq 0 1); do
 #  iperf -c server -u -b 500k -t 90 -i 5 -l 90    > "/tmp/uljitter-$trial.txt"
