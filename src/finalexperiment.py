3ec65c1e32f25ec5ea428feecafd15e22cf3dcb7
Fraida Fund <ffund@nyu.edu>
2017-01-02 20:46:42 -0500

    Change socket buffer sizes in rspec

--- src/finalexperiment.py
+++ src/finalexperiment.py
@@ -218,6 +218,7 @@ for rowindex, houseinfo in housearray.iterrows():
 vms.insert(housecount, servervm)
 
 servervm.addService(PG.Execute(shell="/bin/sh", command="wget -qO- https://raw.githubusercontent.com/csmithsalzberg/CodeRealisticTestbeds/master/util/iperfsetup.sh | bash; iperf3 -s -D; iperf -s -u"))
+servervm.addService(PG.Execute(shell="/bin/sh", command="sudo sysctl -w net.core.rmem_max=134217728; sudo sysctl -w net.core.wmem_max=134217728; sudo sysctl -w net.ipv4.tcp_rmem='4096 87380 67108864'; sudo sysctl -w net.ipv4.tcp_wmem='4096 65536 67108864'"))
 r.addResource(servervm)
 
 # Determining the location for rspec
